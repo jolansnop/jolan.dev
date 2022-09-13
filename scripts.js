c9e750f3fe89620f07e477ec2072a4e63b857083
jolansnop <jasenopati@gmail.com>
2022-09-12 21:46:10 -0600

    large update 09122022. added javascript.

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const catbutton = document.getElementById("catbutton");
+const closebutton = document.getElementById("closebutton");
+const modal = document.getElementById("modal");
+const morebutton = document.getElementById("morebutton");
+
+catbutton.addEventListener("click", () => {
+    modal.classList.add("active");
+});
+
+closebutton.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+morebutton.addEventListener("click", () => {
+    getExtra();
+});
+
+function getExtra() {
+    var xml = new XMLHttpRequest();
+    xml.open("GET", "extra.html", true);
+    xml.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("extrastuff").innerHTML = xml.responseText;
+        }
+    };
+    xml.send();
+}
