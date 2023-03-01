22381ff806823370cf3082c0bb9e7d58675ef2fe
jolansnop <jasenopati@gmail.com>
2023-03-01 07:22:00 -0700

    integrated more React. Also fixed spelling errors. Also less wordy.

--- LearnReact.js
+++ LearnReact.js
@@ -21,3 +21,25 @@ class LearnReact extends React.Component {
         )
     }
 }
+
+class LogoSpin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            speed: 6
+        }
+    }
+
+    render(){
+
+        return (
+            <div>
+            <img onMouseEnter={()=> this.setState({speed: 0.3})}
+             onMouseOut={()=> this.setState({speed: 6})} 
+             style={{animation: `spin ${this.state.speed}s linear infinite`}} 
+             className="imgheader" src="imgs/logo.png" alt="img"/>
+            <h1>Julien Senopati</h1>
+            </div>
+        )
+    }
+}
