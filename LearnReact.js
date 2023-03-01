9d48cd1c9723a19997240a182cdf317b274d5911
jolansnop <jasenopati@gmail.com>
2023-03-01 02:09:20 -0700

    Fixed React code

--- LearnReact.js
+++ LearnReact.js
@@ -1,6 +1,23 @@
 //React below for jolan.dev/react.html
-class LearnReact extends LearnReact.Component {
+class LearnReact extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            clickMe: false
+        }
+    }
+
+    render(){
+
+        if(this.state.clickMe){
+            return (
+            <h2>Powered by React.js</h2>
+            )
+        }
+
+        return (
+            <button onClick={()=> this.setState({clickMe: true})}>Click Me!</button>
+
+        )
     }
 }
